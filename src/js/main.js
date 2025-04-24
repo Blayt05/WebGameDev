985fd0682be7c9201ff71258b6c472734e7b082e
Israel Booz <israelbooz05@gmail.com>
2025-04-24 10:13:53 -0600

    Creacion de estructura

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+
+// Función para cargar un componente HTML en un contenedor
+function loadComponent(containerId, componentPath) {
+    fetch(componentPath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(containerId).innerHTML = data;
+        })
+        .catch(error => console.error(`Error al cargar el componente ${componentPath}:`, error));
+}
+
+// Cargar el componente header.html
+loadComponent('header-container', './src/components/header.html');
