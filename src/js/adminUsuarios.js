98ca308f2a416de206565ee16336e3a2eae6d096
dmelgar1110 <a00839106@tec.mx>
2025-05-14 22:00:58 -0600

    Admin-Usuarios

--- src/js/adminUsuarios.js
+++ src/js/adminUsuarios.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const apiUrl = 'https://localhost:7141/api/Usuarios';
+    const apiUrl = 'https://localhost:7012/api/usuarios';
     const tableBody = document.querySelector('table tbody');
     const panel = document.createElement('div'); // Panel para mostrar información del usuario
     panel.classList.add('user-panel');
     document.body.appendChild(panel);
+    document.querySelector('.create-button').addEventListener('click', crearUsuario);
 
     function fetchUsuarios() {
         fetch(apiUrl)
@@ -26,9 +27,11 @@ document.addEventListener('DOMContentLoaded', function () {
         usuarios.forEach(usuario => {
             const row = document.createElement('tr');
             row.innerHTML = `
-                <td>${usuario.usuario}</td>
+                <td>${usuario.gamerName}</td>
                 <td>${usuario.nombre}</td>
                 <td>${usuario.apellido}</td>
+                <td>${usuario.email}</td>
+                <td>${usuario.rol}</td>
             `;
             row.addEventListener('click', () => abrirPanel(usuario)); // Agrega evento para abrir el panel
             tableBody.appendChild(row);
@@ -41,12 +44,18 @@ document.addEventListener('DOMContentLoaded', function () {
                 <button class="close-panel">X</button>
             </div>
             <div class="panel-content">
-                <label>Usuario:</label>
-                <input type="text" id="usuario" value="${usuario.usuario}" disabled>
+                <label>GamerName:</label>
+                <input type="text" id="gamerName" value="${usuario.gamerName}" disabled>
                 <label>Nombre:</label>
                 <input type="text" id="nombre" value="${usuario.nombre}" disabled>
                 <label>Apellido:</label>
                 <input type="text" id="apellido" value="${usuario.apellido}" disabled>
+                <label>Email:</label>
+                <input type="text" id="email" value="${usuario.email}" disabled>
+                <label>Contraseña:</label>
+                <input type="text" id="password" value="Not Available" disabled>
+                <label>Rol:</label>
+                <input type="text" id="rol" value="${usuario.rol}" disabled>
                 <div class="panel-buttons">
                     <button class="edit-button">Editar</button>
                     <button class="delete-button">Eliminar</button>
@@ -69,45 +78,117 @@ document.addEventListener('DOMContentLoaded', function () {
         panel.style.display = 'none';
     }
 
+    function crearUsuario(){
+        abrirPanel({gamerName: '', nombre: '', apellido: '', email: '', rol:'' })
+        const nombreInput = document.getElementById('nombre');
+        const apellidoInput = document.getElementById('apellido');
+        const gamerNameInput = document.getElementById('gamerName');
+        const emailInput = document.getElementById('email');
+        const passwordInput = document.getElementById('password');
+        const rolInput = document.getElementById('rol');
+        const deleteButton = panel.querySelector('.delete-button');
+        const editButton = panel.querySelector('.edit-button');
+
+        nombreInput.disabled = false;
+        apellidoInput.disabled = false;
+        gamerNameInput.disabled = false;
+        emailInput.disabled = false;
+        passwordInput.disabled = false;
+        rolInput.disabled = false;
+        editButton.textContent = 'Guardar';
+        deleteButton.disabled = true;
+
+
+        editButton.replaceWith(editButton.cloneNode(true)); // Reemplaza el botón para eliminar listeners
+        const newEditButton = panel.querySelector('.edit-button');
+        newEditButton.addEventListener('click', () => guardarNuevoUsuario({
+            //idUsuario: "",
+            nombre: nombreInput.value,
+            apellido: apellidoInput.value,
+            gamername: gamerNameInput.value,
+            email: emailInput.value,
+            password: passwordInput.value,
+            rol: rolInput.value
+        }));
+    }
+
+    function guardarNuevoUsuario(nuevoUsuario) {
+        fetch(`${apiUrl}/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(nuevoUsuario)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error al crear el usuario');
+                }
+                return response.json();
+            })
+            .then(() => {
+                alert('Usuario creado con éxito');
+                cerrarPanel();
+                fetchUsuarios();
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+
     function editarUsuario(usuario) {
         const nombreInput = document.getElementById('nombre');
         const apellidoInput = document.getElementById('apellido');
+        const gamerNameInput = document.getElementById('gamerName');
+        const emailInput = document.getElementById('email');
+        const rolInput = document.getElementById('rol');
         const editButton = panel.querySelector('.edit-button');
 
         if (editButton.textContent === 'Editar') {
             // Habilitar edición
             nombreInput.disabled = false;
             apellidoInput.disabled = false;
+            gamerNameInput.disabled = false;
+            emailInput.disabled = false;
+            rolInput.disabled = false;
             editButton.textContent = 'Guardar';
         } else {
             // Guardar cambios
             const updatedUsuario = {
                 ...usuario,
                 nombre: nombreInput.value,
-                apellido: apellidoInput.value
+                apellido: apellidoInput.value,
+                gamername: gamerNameInput.value,
+                email: emailInput.value,
+                rol: rolInput.value
             };
 
-            fetch(`${apiUrl}/${usuario.id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(updatedUsuario)
-            })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Error al actualizar el usuario');
-                    }
-                    return response.json();
+            editButton.replaceWith(editButton.cloneNode(true)); // Reemplaza el botón para eliminar listeners
+            const newEditButton = panel.querySelector('.edit-button');
+
+            newEditButton.addEventListener('click', () => {
+                fetch(`${apiUrl}/${usuario.id}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(updatedUsuario)
                 })
-                .then(() => {
-                    alert('Usuario actualizado con éxito');
-                    cerrarPanel();
-                    fetchUsuarios(); // Actualiza la tabla
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Error al actualizar el usuario');
+                        }
+                        return response.ok;
+                    })
+                    .then(() => {
+                        alert('Usuario actualizado con éxito');
+                        cerrarPanel();
+                        fetchUsuarios(); // Actualiza la tabla
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                    });
+            });
         }
     }
 
@@ -131,11 +212,11 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     const usuarios = [
-        { id: 1, usuario: 'user1', nombre: 'Juan', apellido: 'Pérez' },
-        { id: 2, usuario: 'user2', nombre: 'Ana', apellido: 'García' },
-        { id: 3, usuario: 'user3', nombre: 'Luis', apellido: 'Martínez' },
-        { id: 4, usuario: 'user4', nombre: 'María', apellido: 'López' }
+        { id: 1, usuario: 'user1', nombre: 'Juan', apellido: 'Pérez', correo: 'd@gmail', rol:'admin' },
+        { id: 2, usuario: 'user2', nombre: 'Ana', apellido: 'García', correo: 'dvewdgmail', rol:'usuario' },
+        { id: 3, usuario: 'user3', nombre: 'Luis', apellido: 'Martínez', correo: 'drewrg@gmail', rol:'usuario' },
+        { id: 4, usuario: 'user4', nombre: 'María', apellido: 'López', correo: 'bgfbgfd@gmail', rol:'usuario' }
     ];
-    //fetchUsuarios();
-    llenarTabla(usuarios);
+    fetchUsuarios();
+    //llenarTabla(usuarios);
 });
